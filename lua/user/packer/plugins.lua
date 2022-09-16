ca8f7030af72baa50a429a2861c379b3a6afbf36
Wolowit <apinant-moo@tutanota.com>
2022-09-16 22:06:57 +0700

    chore

--- lua/user/packer/plugins.lua
+++ lua/user/packer/plugins.lua
@@ -16,12 +16,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- vim.cmd([[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]])
+
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
@@ -191,3 +192,6 @@ return packer.startup(function(use)
 		require("packer").sync()
 	end
 end)
+
+-- edluffy/hologram.nvim
+-- samodostal/image.nvim

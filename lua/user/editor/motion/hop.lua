386fd7137b76ec832c4a72c37e792ee3f25f644b
LoneExile <apinant-moo@tutanota.com>
2022-11-10 10:52:58 +0700

    feat: unmap hop f and t key

--- lua/user/editor/motion/hop.lua
+++ lua/user/editor/motion/hop.lua
@@ -2,10 +2,10 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 local hopMap = '<cmd>lua require"hop".hint_char1({ direction = require"hop.hint".'
 
-keymap('', 'f', hopMap .. 'HintDirection.AFTER_CURSOR, current_line_only = true })<cr>', opts)
-keymap('', 'F', hopMap .. 'HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>', opts)
-keymap('', 't', hopMap .. 'HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>', opts)
-keymap('', 'T', hopMap .. 'HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>', opts)
+-- keymap('', 'f', hopMap .. 'HintDirection.AFTER_CURSOR, current_line_only = true })<cr>', opts)
+-- keymap('', 'F', hopMap .. 'HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>', opts)
+-- keymap('', 't', hopMap .. 'HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>', opts)
+-- keymap('', 'T', hopMap .. 'HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>', opts)
 
 keymap('n', 'gh', '<CMD>HopWord<CR>', opts)
 

2e5a6cfdc77ab618f1e60d77f1cc00d6b80dbf8a
LoneExile <apinant-moo@tutanota.com>
2022-12-27 16:02:20 +0700

    fix: git action [2]

--- lua/user/ui/alpha.lua
+++ lua/user/ui/alpha.lua
@@ -60,7 +60,7 @@ dashboard.section.buttons.val = {
 }
 
 local function info()
-  local plugins = require("lazy").stats().count
+  local plugins = require('lazy').stats().count
   local v = vim.version()
   local datetime = os.date(' %d-%m-%Y   %H:%M:%S')
   local platform = vim.fn.has('win32') == 1 and '' or ''

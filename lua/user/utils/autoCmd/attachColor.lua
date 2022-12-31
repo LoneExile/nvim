c3251a47822e0bcc7fd25763b50ad9d7c5d998f0
LoneExile <apinant-moo@tutanota.com>
2022-12-31 11:04:57 +0700

    feat: add chatGPT keymap, update etc.

--- lua/user/utils/autoCmd/attachColor.lua
+++ lua/user/utils/autoCmd/attachColor.lua
@@ -3,14 +3,11 @@ local group = vim.api.nvim_create_augroup('colorAttach', { clear = true })
 local events = { 'BufEnter', 'BufWinEnter', 'CursorMoved' }
 vim.api.nvim_create_autocmd(events, {
   pattern = { '*' },
-  -- command = ":ColorizerAttachToBuffer",
   callback = function()
-    -- vim.cmd('ColorizerAttachToBuffer')
     vim.cmd([[
     if exists('g:colorizer')
       ColorizerAttachToBuffer
     ]])
-    -- vim.cmd('Neotree close')
   end,
   group = group,
 })

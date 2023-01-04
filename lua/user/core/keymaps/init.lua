fb707f1ac6ed579886dd87bc8ffaeef683ae32aa
LoneExile <apinant-moo@tutanota.com>
2023-01-04 13:04:55 +0700

    refactor: remove unnecessary code

--- lua/user/core/keymaps/init.lua
+++ lua/user/core/keymaps/init.lua
@@ -1,6 +1,6 @@
 local location = 'user.core.keymaps.'
 
-require(location .. 'key_general').load_defaults()
+require(location .. 'key_general')
 require(location .. 'key_terminal')
 require(location .. 'whichKey')
 -- require(location .. 'reload')

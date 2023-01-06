9d1c6555c528887c20bb5f8417303dacfe2c0f4c
LoneExile <apinant-moo@tutanota.com>
2023-01-06 11:06:04 +0700

    feat: update setup gpt .env file

--- lua/user/lsp/ai/chatGPT.lua
+++ lua/user/lsp/ai/chatGPT.lua
@@ -1,8 +1,9 @@
 local M = {}
 
-local env = vim.fn.stdpath('config') .. '/env.lua'
+local env = vim.fn.stdpath('config') .. '/.env.lua'
 
-if vim.fn.filereadable(env) == 0 then
+-- if you don't export the env in your shell, it will generate file .env.lua in your config folder
+if vim.fn.filereadable(env) == 0 and vim.env.OPENAI_API_KEY == nil then
   local value = '-- https://beta.openai.com/account/api-keys\n vim.cmd(\'let $OPENAI_API_KEY="<PUT-YOUR-KEY-HERE>"\')'
   io.open(env, 'w'):write(value)
   io.open(env, 'w'):close()

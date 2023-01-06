ac82536d5a9c1914c42f26d1da27e70f7c7b8a5a
LoneExile <apinant-moo@tutanota.com>
2023-01-06 10:33:28 +0700

    chore: update

--- init.lua
+++ init.lua
@@ -12,4 +12,7 @@ require('user.ui')
 
 ---------------------------------
 
--- FIX: startup time
+-- FIX: startup time, restucture plugins by groups (lsps, treesitter, utils, etc)
+-- minimal config to run nvim
+-- full config to run nvim
+-- whichkey better loading time when changing filetypes

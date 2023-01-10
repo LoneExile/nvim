20e5fe36adb8a12d6175231cfbf09e84c0a185da
LoneExile <apinant-moo@tutanota.com>
2023-01-10 12:49:09 +0700

    feat: remove tab key cmp

--- lua/user/lsp/cmp/keymaps.lua
+++ lua/user/lsp/cmp/keymaps.lua
@@ -81,29 +81,29 @@ M.keymaps = {
       end
     end,
   }),
-  ['<Tab>'] = cmp.mapping(function(fallback)
-    if cmp.visible() and has_words_before() then
-      -- if cmp.visible() then
-      cmp.select_next_item()
-    elseif luasnip.expand_or_locally_jumpable() then
-      luasnip.expand_or_jump()
-    elseif jumpable(1) then
-      luasnip.jump(1)
-    elseif has_words_before() then
-      cmp.complete()
-    else
-      fallback()
-    end
-  end, { 'i', 's' }),
-  ['<S-Tab>'] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_prev_item()
-    elseif luasnip.jumpable(-1) then
-      luasnip.jump(-1)
-    else
-      fallback()
-    end
-  end, { 'i', 's' }),
+  -- ['<Tab>'] = cmp.mapping(function(fallback)
+  --   if cmp.visible() and has_words_before() then
+  --     -- if cmp.visible() then
+  --     cmp.select_next_item()
+  --   elseif luasnip.expand_or_locally_jumpable() then
+  --     luasnip.expand_or_jump()
+  --   elseif jumpable(1) then
+  --     luasnip.jump(1)
+  --   elseif has_words_before() then
+  --     cmp.complete()
+  --   else
+  --     fallback()
+  --   end
+  -- end, { 'i', 's' }),
+  -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+  --   if cmp.visible() then
+  --     cmp.select_prev_item()
+  --   elseif luasnip.jumpable(-1) then
+  --     luasnip.jump(-1)
+  --   else
+  --     fallback()
+  --   end
+  -- end, { 'i', 's' }),
   ['<C-Space>'] = cmp.mapping.complete(),
   ['<C-e>'] = cmp.mapping.abort(),
   ['<CR>'] = cmp.mapping(function(fallback)

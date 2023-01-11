45cc5f5ec41e88480b04daf22ce8bf8bd90ce5f0
LoneExile <apinant-moo@tutanota.com>
2023-01-11 10:05:20 +0700

    feat: move todo to readme, add web-search cmd

--- init.lua
+++ init.lua
@@ -11,8 +11,3 @@ require('user.editor')
 require('user.ui')
 
 ---------------------------------
-
--- FIX: startup time, restucture plugins by groups (lsps, treesitter, utils, etc)
--- minimal config to run nvim
--- full config to run nvim
--- whichkey better loading time when changing filetypes

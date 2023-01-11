2209e1bdc548bdefecd5730272f5cb8893723bfd
LoneExile <apinant-moo@tutanota.com>
2023-01-11 12:00:11 +0700

    fix: git action lint

--- lua/user/utils/autoCmd/web-search.lua
+++ lua/user/utils/autoCmd/web-search.lua
@@ -44,7 +44,7 @@ cmd('Search', function()
           end,
         }),
         sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr, map)
+        attach_mappings = function(prompt_bufnr)
           actions.select_default:replace(function()
             actions.close(prompt_bufnr)
             local selection = action_state.get_selected_entry()

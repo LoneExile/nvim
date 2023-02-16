8624334b57dabc8dcbab91e18f748dcd95da2460
LoneExile <apinant-moo@tutanota.com>
2023-02-16 11:16:02 +0700

    feat: add pets.nvim

--- lua/user/core/lazy.lua
+++ lua/user/core/lazy.lua
@@ -27,11 +27,30 @@ local plugins = {
   --   end,
   -- },
 
+  {
+    'giusgad/pets.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'edluffy/hologram.nvim' },
+    config = function()
+      require('pets').setup({
+        {
+          row = 1, -- the row (height) to display the pet at (must be at least 1 and at most 10)
+          col = 0, -- the column to display the pet at (set to high number to have it stay still on the right side)
+          speed_multiplier = 1, -- you can make your pet move faster/slower. If slower the animation will have lower fps.
+          default_pet = 'cat', -- the pet to use for the PetNew command
+          default_style = 'brown', -- the style of the pet to use for the PetNew command
+          random = false, -- wether to use a random pet for the PetNew command, ovverides default_pet and default_style
+          death_animation = true, -- animate the pet's death, set to false to feel less guilt
+        },
+      })
+    end,
+  },
+
   -- Treesitter
   {
     'nvim-treesitter/nvim-treesitter',
     event = 'BufRead',
     build = function()
+      ---@diagnostic disable-next-line: param-type-mismatch
       pcall(vim.cmd, 'TSUpdate')
     end,
   },

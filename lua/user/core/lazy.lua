d12fffbde9854aecf322f8746978d3e509db6c5f
LoneExile <apinant-moo@tutanota.com>
2023-02-17 23:59:21 +0700

    dev: bad commit

--- lua/user/core/lazy.lua
+++ lua/user/core/lazy.lua
@@ -31,17 +31,7 @@ local plugins = {
     'giusgad/pets.nvim',
     dependencies = { 'MunifTanjim/nui.nvim', 'edluffy/hologram.nvim' },
     config = function()
-      require('pets').setup({
-        {
-          row = 1, -- the row (height) to display the pet at (must be at least 1 and at most 10)
-          col = 0, -- the column to display the pet at (set to high number to have it stay still on the right side)
-          speed_multiplier = 1, -- you can make your pet move faster/slower. If slower the animation will have lower fps.
-          default_pet = 'cat', -- the pet to use for the PetNew command
-          default_style = 'brown', -- the style of the pet to use for the PetNew command
-          random = false, -- wether to use a random pet for the PetNew command, ovverides default_pet and default_style
-          death_animation = true, -- animate the pet's death, set to false to feel less guilt
-        },
-      })
+      require('pets').setup({})
     end,
   },
 
@@ -222,6 +212,9 @@ local plugins = {
       { 'saadparwaiz1/cmp_luasnip' },
       { 'jcha0713/cmp-tw2css' },
     },
+    config = function()
+      require('user.lsp.cmp')
+    end,
   },
 
   -- Snippets

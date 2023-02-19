e521ae2d47082424f4e7527665faaf56a1ea18ff
LoneExile <apinant-moo@tutanota.com>
2023-02-19 15:23:51 +0700

    draft: remove unuse in init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,2 @@
--- check requirements
 require('user.core')
 require('user.utils.requirements')
-
--- require('user.core.lazy')
-
--- ------------------------------------------------------------------
--- require('user.utils.autoCmd')
--- require('user.core')
--- require('user.lsp')
--- require('user.dap')
--- require('user.editor')
--- require('user.ui')
-
----------------------------------

058fcb1c09e260b57539371b550fe83bad7d01f6
LoneExile <apinant-moo@tutanota.com>
2023-02-19 22:42:39 +0700

    feat: improve startup time tsserver, lua_ls

--- lua/user/lsp/servers/addon/schemastore.lua
+++ lua/user/lsp/servers/addon/schemastore.lua
@@ -3,6 +3,8 @@ local M = {}
 M.setup = function()
   return {
     'b0o/schemastore.nvim',
+    enabled = true,
+    lazy = true,
   }
 end
 
--- lua/user/lsp/servers/addon/typescript.lua
+++ lua/user/lsp/servers/addon/typescript.lua
@@ -3,32 +3,33 @@ local M = {}
 M.setup = function()
   return {
     'jose-elias-alvarez/typescript.nvim',
-    enabled = true,
     dependencies = { 'SmiteshP/nvim-navic' },
+    lazy = true,
+    ft = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
 
-    config = function()
-      local status, typescript = pcall(require, 'typescript')
-      if not status then
-        return
-      end
-      local status_ok, navic = pcall(require, 'nvim-navic')
-      if not status_ok then
-        return
-      end
-      typescript.setup({
-        disable_commands = false, -- prevent the plugin from creating Vim commands
-        debug = false, -- enable debug logging for commands
-        go_to_source_definition = {
-          fallback = true, -- fall back to standard LSP definition on failure
-        },
-        server = { -- pass options to lspconfig's setup method
-          on_attach = function(client, bufnr)
-            navic.attach(client, bufnr)
-            vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-          end,
-        },
-      })
-    end,
+    -- config = function()
+    --   local status, typescript = pcall(require, 'typescript')
+    --   if not status then
+    --     return
+    --   end
+    --   local status_ok, navic = pcall(require, 'nvim-navic')
+    --   if not status_ok then
+    --     return
+    --   end
+    --   typescript.setup({
+    --     disable_commands = false, -- prevent the plugin from creating Vim commands
+    --     debug = false, -- enable debug logging for commands
+    --     go_to_source_definition = {
+    --       fallback = true, -- fall back to standard LSP definition on failure
+    --     },
+    --     server = { -- pass options to lspconfig's setup method
+    --       on_attach = function(client, bufnr)
+    --         navic.attach(client, bufnr)
+    --         vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+    --       end,
+    --     },
+    --   })
+    -- end,
   }
 end
 

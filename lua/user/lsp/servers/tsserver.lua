946f66e2dd13e3aa41048c56cc9fd548fe3437ac
LoneExile <apinant-moo@tutanota.com>
2023-02-25 12:09:01 +0700

    feat: unuse navic

--- lua/user/lsp/servers/tsserver.lua
+++ lua/user/lsp/servers/tsserver.lua
@@ -36,10 +36,6 @@ M.setup = function(_)
   if not status then
     return
   end
-  local status_ok, navic = pcall(require, 'nvim-navic')
-  if not status_ok then
-    return
-  end
 
   typescript.setup({
     disable_commands = false, -- prevent the plugin from creating Vim commands
@@ -49,6 +45,10 @@ M.setup = function(_)
     },
     server = { -- pass options to lspconfig's setup method
       on_attach = function(client, bufnr)
+        local status_ok, navic = pcall(require, 'nvim-navic')
+        if not status_ok then
+          return
+        end
         navic.attach(client, bufnr)
         vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
       end,

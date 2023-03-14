d9a8fe058dfcd00a4cbc60afdd9d343a5493cf9a
LoneExile <apinant-moo@tutanota.com>
2023-03-14 14:02:18 +0700

    prep: lsp keymaps

--- lua/user/lsp/settings/keymaps.lua
+++ lua/user/lsp/settings/keymaps.lua
@@ -1,15 +1,28 @@
 local M = {}
 
 -- TODO: if lspsaga not load use default
+-- ('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+-- ('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+-- ('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+-- ('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+-- ('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+-- ('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+-- ('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+-- ('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+-- ('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
+-- ('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+-- ('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+-- ('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+-- ('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+-- ('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
 local buffer_mappings = {
   normal_mode = {
-    -- ['K'] = { '<cmd>Lspsaga hover_doc<CR>', 'Show hover' },
     ['K'] = { '<cmd>Lspsaga hover_doc<CR>', 'Show hover' },
     ['gd'] = { '<cmd>Lspsaga peek_definition<CR>', 'Goto Definition' },
     ['gs'] = { vim.lsp.buf.signature_help, 'show signature help' },
     ['gr'] = { '<cmd>Lspsaga lsp_finder<CR>', 'Goto references' },
     ['gR'] = { '<cmd>Lspsaga rename<CR>', 'rename' },
-    -- ['gq'] = { '<cmd>lua vim.lsp.buf.code_action()<CR>', 'Code action' },
     ['gq'] = { '<cmd>Lspsaga code_action<CR>', 'Code action' },
     ['gj'] = { '<cmd>Lspsaga diagnostic_jump_next<CR>', 'Next diagnostic' },
     ['gk'] = { '<cmd>Lspsaga diagnostic_jump_prev<CR>', 'Prev diagnostic' },

85486d8d58675cf0420b1189a9bf97e398004d82
LoneExile <Apinant-Moo@tutanota.com>
2023-04-12 11:15:52 +0700

    fix: bug mapping which-key, move mapping comment to plugin config file

--- lua/user/editor/treesitter/comment.lua
+++ lua/user/editor/treesitter/comment.lua
@@ -1,5 +1,34 @@
 local M = {}
 
+M.enabled = true
+
+M.wh_key = {
+  enabled = M.enabled,
+  wh_mappings = {
+    ['/'] = {
+      cmd = {
+        -- n = "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
+        -- v = '<Plug>(comment_toggle_linewise_visual)',
+        n = function()
+          local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+          local api = require('Comment.api')
+          vim.api.nvim_feedkeys(esc, 'nx', false)
+          api.toggle.linewise.current()
+        end,
+        v = function()
+          local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+          local api = require('Comment.api')
+          vim.api.nvim_feedkeys(esc, 'nx', false)
+          api.toggle.blockwise(vim.fn.visualmode())
+        end,
+      },
+      'Comment',
+      mode = { 'n', 'v' },
+      ft = { only = {}, exclude = { 'alpha', 'neo-tree' } },
+    },
+  },
+}
+
 M.setup = function()
   return {
     'numToStr/Comment.nvim',
@@ -7,7 +36,7 @@ M.setup = function()
     dependencies = {
       'JoosepAlviste/nvim-ts-context-commentstring',
     },
-    enabled = true,
+    enabled = M.enabled,
     config = function()
       local status_ok, comment = pcall(require, 'Comment')
       if not status_ok then

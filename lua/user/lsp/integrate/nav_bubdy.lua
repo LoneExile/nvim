bb46440b4f5a010893225c5c41d2956950144afc
LoneExile <apinant-moo@tutanota.com>
2023-04-18 10:46:00 +0700

    feat: add function able to use nav_buddy and navic

--- lua/user/lsp/integrate/nav_bubdy.lua
+++ lua/user/lsp/integrate/nav_bubdy.lua
@@ -6,7 +6,13 @@ M.setup = function(_, _)
   return {
     'SmiteshP/nvim-navbuddy',
     enabled = M.enabled,
-    dependencies = { 'neovim/nvim-lspconfig', 'SmiteshP/nvim-navic', 'MunifTanjim/nui.nvim' },
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'SmiteshP/nvim-navic',
+      'MunifTanjim/nui.nvim',
+      'numToStr/Comment.nvim', -- Optional
+      'nvim-telescope/telescope.nvim', -- Optional
+    },
     config = function()
       local status_ok, navbuddy = pcall(require, 'nvim-navbuddy')
       if not status_ok then
@@ -17,12 +23,5 @@ M.setup = function(_, _)
     end,
   }
 end
-
--- on_attach = function(client, bufnr)
---   local status_navbuddy, navbuddy = pcall(require, 'nvim-nvim-navbuddy')
---   if status_navbuddy then
---     navbuddy.attach(client, bufnr)
---   end
--- end,
 
 return M

0717f0eafac27b714853a8f7b902968616b4a1b3
LoneExile <Apinant-Moo@tutanota.com>
2023-04-19 19:04:47 +0700

    fix: rm unuse req autocmd

--- lua/user/core/autocmds/init.lua
+++ lua/user/core/autocmds/init.lua
@@ -1,5 +1,4 @@
 require('user.core.autocmds.saveAndFormat')
 require('user.core.autocmds.yank')
-require('user.core.autocmds.lsp')
 require('user.core.autocmds.ftplugin')
 require('user.core.autocmds.auto_create_dir')

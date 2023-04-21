4dc5278dcc466f7a4bd9402d20e04a95b1856e9b
LoneExile <apinant-moo@tutanota.com>
2023-04-21 14:41:48 +0700

    feat: new way to setup nvim (1)

--- lua/user/core/init.lua
+++ lua/user/core/init.lua
@@ -1,7 +1,17 @@
-require('user.utils.requirements')
+local M = {}
 
-require('user.core.optimize')
-require('user.core.options')
-require('user.core.keymaps')
-require('user.core.lazy')
-require('user.core.autocmds')
+M.setup = function(root)
+  local _, main = pcall(require, root)
+  local current_loc = main.get_current_script_path()
+
+  -- used to remove the last path component (the file or directory name)
+  current_loc = vim.fn.fnamemodify(current_loc, ':h')
+
+  local exclude = { 'settings' }
+  local files = main.get_files_in_dir(current_loc)
+  files = main.clean_path(files, exclude)
+
+  require(root).load_config(files)
+end
+
+return M
---
+++ lua/user/init.lua
@@ -0,0 +1,75 @@
+local M = {}
+
+M.get_current_script_path = function(stack_level)
+  stack_level = stack_level or 2
+  local info = debug.getinfo(stack_level, 'S')
+  local path = info and info.source:sub(2) or nil
+  return path
+end
+
+M.get_root_config = function(path)
+  local root_config = path and path:match('lua/(.-)/') or nil
+  return root_config
+end
+
+M.get_current_file_path = function()
+  local path = M.get_current_script_path(3)
+  local file_path = path and path:match('lua/(.+)') or nil
+  return file_path
+end
+
+M.load_config = function(modules)
+  local path = M.get_current_script_path()
+  local root = M.get_root_config(path)
+  for _, module in ipairs(modules) do
+    print('Loading module: ' .. module)
+    local loaded_module = require(root .. '.' .. module)
+    local _, _ = pcall(function()
+      loaded_module.setup(root)
+    end)
+  end
+end
+
+M.setup = function()
+  M.load_config(M.core)
+end
+
+M.core = { 'core' }
+
+M.get_files_in_dir = function(dir)
+  local files = vim.fn.glob(dir .. '/*', false, true)
+  return files
+end
+
+M.is_excluded = function(path, exclude_list)
+  for _, pattern in ipairs(exclude_list) do
+    if path:find(pattern .. '$') then
+      return true
+    end
+  end
+  return false
+end
+
+M.clean_path = function(files, exclude_list)
+  local paths = {}
+  local replacements = {
+    { search = '/', replace = '.' },
+    { search = '%.lua$', replace = '' },
+    { search = '^user%.', replace = '' },
+  }
+
+  for _, file in ipairs(files) do
+    local path = file:match('nvim/lua/(.+)')
+    if not M.is_excluded(path, exclude_list) then
+      for _, replacement in ipairs(replacements) do
+        path = path:gsub(replacement.search, replacement.replace)
+      end
+      if not path:match('init$') then -- Exclude files that end with 'init'
+        table.insert(paths, path)
+      end
+    end
+  end
+  return paths
+end
+
+return M

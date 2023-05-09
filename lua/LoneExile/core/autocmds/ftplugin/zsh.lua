7d192457d806f40c37e1321467fec41511ab33cb
LoneExile <Apinant-Moo@tutanota.com>
2023-05-09 14:02:18 +0700

    feat: add obsidian.nvim

--- lua/LoneExile/core/autocmds/ftplugin/zsh.lua
+++ lua/LoneExile/core/autocmds/ftplugin/zsh.lua
@@ -1,6 +1,6 @@
 local group = vim.api.nvim_create_augroup('autocmd_zsh', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'zsh',
+  pattern = { 'zsh', 'sh' },
   callback = function()
     require('nvim-treesitter.highlight').attach(0, 'bash')
   end,

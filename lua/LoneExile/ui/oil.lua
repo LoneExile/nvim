22701d47a80dbd30b1153173292914e967f3395f
LoneExile <Apinant-Moo@tutanota.com>
2023-07-15 17:16:29 +0700

    feat: enabled Oil, Hop `fFtT` key

--- lua/LoneExile/ui/oil.lua
+++ lua/LoneExile/ui/oil.lua
@@ -1,8 +1,34 @@
 local M = {}
 
+M.wh_key = {
+  wh_mappings = {
+    u = {
+      name = '+Utility',
+      o = {
+        function()
+          vim.cmd('Oil --float')
+        end,
+        'Oil',
+        mode = { 'n' },
+      },
+    },
+  },
+}
+
+vim.keymap.set(
+  'n',
+  '-',
+  -- require('oil').open
+  function()
+    vim.cmd('Oil --float')
+  end,
+  { desc = 'Open parent directory' }
+)
+
 M.setup = function()
   return {
     'stevearc/oil.nvim',
+    cmd = { 'Oil' },
     config = function()
       local status_ok_code, spectre = pcall(require, 'oil')
       if not status_ok_code then
@@ -24,6 +50,21 @@ M.setup = function()
           ['`'] = 'actions.cd',
           ['~'] = 'actions.tcd',
           ['g.'] = 'actions.toggle_hidden',
+        },
+        float = {
+          -- Padding around the floating window
+          padding = 2,
+          max_width = 50,
+          max_height = 40,
+          border = 'rounded',
+          win_options = {
+            winblend = 10,
+          },
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          override = function(conf)
+            return conf
+          end,
         },
       }
 

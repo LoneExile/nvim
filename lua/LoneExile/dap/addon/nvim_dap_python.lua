ff14023b9a5a4e266477f6738dadba913f11204d
LoneExile <Apinant-Moo@tutanota.com>
2023-07-17 21:59:19 +0700

    feat: update dap python

--- lua/LoneExile/dap/addon/nvim_dap_python.lua
+++ lua/LoneExile/dap/addon/nvim_dap_python.lua
@@ -1,12 +1,13 @@
 local M = {}
 
-M.setup = function()
+M.setup = function(s, _)
   return {
     'mfussenegger/nvim-dap-python',
     dependencies = { 'mfussenegger/nvim-dap' },
     ft = { 'python' },
     config = function()
-      require('dap-python').setup('~/.pyenv/shims/python')
+      local debug_adapter = vim.fn.glob(s.data_loc .. '/mason/') .. 'packages/debugpy/venv/bin/python3'
+      require('dap-python').setup(debug_adapter)
     end,
   }
 end

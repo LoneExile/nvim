25a490388d695bd967744deb931975e94619267d
LoneExile <Apinant-Moo@tutanota.com>
2023-07-20 08:49:41 +0700

    minor

--- lua/LoneExile/lsp/servers/addon/tailwind/sorter.lua
+++ lua/LoneExile/lsp/servers/addon/tailwind/sorter.lua
@@ -8,7 +8,17 @@ M.setup = function()
     config = function()
       require('tailwind-sorter').setup({
         on_save_enabled = true, -- If `true`, automatically enables on save sorting.
-        on_save_pattern = { '*.html', '*.js', '*.jsx', '*.tsx', '*.twig', '*.hbs', '*.php', '*.heex', '*.astro' }, -- The file patterns to watch and sort.
+        on_save_pattern = {
+          '*.html',
+          '*.js',
+          '*.jsx',
+          '*.tsx',
+          '*.twig',
+          '*.hbs',
+          '*.php',
+          '*.heex',
+          '*.astro',
+        }, -- The file patterns to watch and sort.
         node_path = 'node',
       })
     end,

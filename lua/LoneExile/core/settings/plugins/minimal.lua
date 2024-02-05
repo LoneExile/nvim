f8a451bf704f9f25bcab54622408818a9cf092ab
Apinant.U <Apinant-Moo@tutanota.com>
2024-02-05 18:00:55 +0700

    feat(plugins-list): minimal

---
+++ lua/LoneExile/core/settings/plugins/minimal.lua
@@ -0,0 +1,35 @@
+-- stylua: ignore
+
+return {
+  -- CMP
+  { 'hrsh7th/nvim-cmp',                          true },
+
+  -- LSP
+  { 'neovim/nvim-lspconfig',                     true },
+  -- { 'j-hui/fidget.nvim',                         false },
+  -- { 'glepnir/lspsaga.nvim',                      true },
+  { 'williamboman/mason.nvim',                   true },
+  { 'williamboman/mason-lspconfig.nvim',         true },
+  -- { 'WhoIsSethDaniel/mason-tool-installer.nvim', true },
+  { 'folke/lua-dev.nvim',                        true },
+
+  -- Editor
+  -- { 'akinsho/toggleterm.nvim',                   true },
+
+  -- Tool
+  { 'nvim-telescope/telescope.nvim',             true },
+
+  -- Treesitter
+  { 'numToStr/Comment.nvim',                     true },
+  { 'nvim-treesitter/nvim-treesitter',           true },
+  { 'nvim-treesitter/nvim-treesitter-context',   true },
+  { 'lukas-reineke/indent-blankline.nvim',       true },
+
+  -- UI
+  { 'goolord/alpha-nvim',                        true },
+  { 'folke/tokyonight.nvim',                     true },
+  { 'nvim-lualine/lualine.nvim',                 true },
+  -- { 'nvim-neo-tree/neo-tree.nvim',               true },
+  { 'rcarriga/nvim-notify',                      true },
+  { 'folke/which-key.nvim',                      true },
+}

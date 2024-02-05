c7cd5c432b1ab76a2ebf7f1c8a08f601a7e79cae
Apinant.U <Apinant-Moo@tutanota.com>
2024-02-05 17:50:57 +0700

    fix(action): format

--- lua/LoneExile/utils/requirements.lua
+++ lua/LoneExile/utils/requirements.lua
@@ -1,7 +1,7 @@
 if vim.fn.has('nvim-0.9') ~= 1 then
   vim.api.nvim_echo({
     { 'Please upgrade your Neovim base installation.requires v0.9+', 'ErrorMsg' },
-    { 'Press any key to continue',                                   'WarningMsg' },
+    { 'Press any key to continue', 'WarningMsg' },
   }, true, {})
   vim.fn.getchar()
   vim.cmd([[quit]])

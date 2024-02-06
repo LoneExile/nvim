3b3674e4285a60890e98d45938aeec4c9b9ce396
Apinant.U <Apinant-Moo@tutanota.com>
2024-02-06 15:33:51 +0700

    fix(dap): python path

--- lua/LoneExile/dap/addon/nvim_dap_python.lua
+++ lua/LoneExile/dap/addon/nvim_dap_python.lua
@@ -9,7 +9,7 @@ M.setup = function(s, _)
     },
     ft = { 'python' },
     config = function()
-      local debug_adapter = vim.fn.glob(s.data_loc .. '/mason/') .. 'packages/debugpy/venv/bin/python3'
+      local debug_adapter = s.utils.convert_path(vim.fn.glob(s.data_loc .. '/mason/') .. 'packages/debugpy/venv/bin/python3')
       require('dap-python').setup(debug_adapter)
     end,
   }

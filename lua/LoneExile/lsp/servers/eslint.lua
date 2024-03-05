69c767c4d2bb2c5c90bb5028c6ca3ddd4141a1fe
Apinant.U <Apinant-Moo@tutanota.com>
2024-03-05 19:31:53 +0700

    feat(lsp): eslint-lsp

--- lua/LoneExile/lsp/servers/eslint.lua
+++ lua/LoneExile/lsp/servers/eslint.lua
@@ -2,30 +2,31 @@ local M = {}
 
 M.setup = function(lspconfig)
   lspconfig.eslint.setup({
-    cmd = { 'vscode-eslint-language-server', '--stdio' },
-    root_dir = lspconfig.util.root_pattern('.eslintrc', '.eslintrc.js', '.eslintrc.json', '.eslintrc.ts'),
-    settings = {
-      format = {
-        enable = true,
-      },
-    },
+    -- cmd = { 'vscode-eslint-language-server', '--stdio' },
+    -- filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
+    -- root_dir = lspconfig.util.root_pattern('.eslintrc', '.eslintrc.js', '.eslintrc.json', '.eslintrc.ts'),
+    -- settings = {
+    --   format = {
+    --     enable = true,
+    --   },
+    -- },
     on_attach = function(_, bufnr)
       vim.api.nvim_create_autocmd('BufWritePre', {
         buffer = bufnr,
         command = 'EslintFixAll',
       })
     end,
-    handlers = {
-      -- this error shows up occasionally when formatting
-      -- formatting actually works, so this will supress it
-      ['window/showMessageRequest'] = function(_, result)
-        if result.message:find('ENOENT') then
-          return vim.NIL
-        end
+    -- handlers = {
+    --   -- this error shows up occasionally when formatting
+    --   -- formatting actually works, so this will supress it
+    --   ['window/showMessageRequest'] = function(_, result)
+    --     if result.message:find('ENOENT') then
+    --       return vim.NIL
+    --     end
 
-        return vim.lsp.handlers['window/showMessageRequest'](nil, result)
-      end,
-    },
+    --     return vim.lsp.handlers['window/showMessageRequest'](nil, result)
+    --   end,
+    -- },
   })
 end
 

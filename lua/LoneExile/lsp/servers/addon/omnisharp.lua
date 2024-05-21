4e2caebe5e9fbfc87911865eb5ee9618c7140f04
LoneExile <Apinant-Moo@tutanota.com>
2024-05-21 10:23:15 +0700

    fix(lsp): cs

--- lua/LoneExile/lsp/servers/addon/omnisharp.lua
+++ lua/LoneExile/lsp/servers/addon/omnisharp.lua
@@ -2,7 +2,9 @@ local M = {}
 
 M.setup = function()
   return {
-    'Hoffs/omnisharp-extended-lsp.nvim',
+    -- 'Hoffs/omnisharp-extended-lsp.nvim',
+    'LoneExile/omnisharp-extended-lsp.nvim',
+    branch = 'fix-remove-deprecated-functions',
     ft = { 'cs' },
   }
 end

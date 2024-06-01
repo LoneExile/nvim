ebaddcc34451e1536ed138581550954022b630f6
LoneExile <Apinant-Moo@tutanota.com>
2024-06-01 17:16:05 +0700

    feat(dadbod): lazyload dadbod

--- lua/LoneExile/editor/db.lua
+++ lua/LoneExile/editor/db.lua
@@ -14,17 +14,14 @@ print(vim.fn.stdpath('data'))
 M.setup = function()
   return {
     'kristijanhusak/vim-dadbod-ui',
+    cmd = {
+      'DBUIToggle',
+    },
     dependencies = {
-      { 'tpope/vim-dadbod', lazy = true },
+      { 'tpope/vim-dadbod', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
       { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
     },
-    cmd = {
-      'DBUI',
-      'DBUIToggle',
-      'DBUIAddConnection',
-      'DBUIFindBuffer',
-    },
-    init = function()
+    config = function()
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.db_ui_win_position = 'right'
       vim.g.db_ui_save_location = vim.fn.stdpath('config') .. '/dadbod_queries'

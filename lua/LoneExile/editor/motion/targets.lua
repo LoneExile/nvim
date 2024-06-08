de4272bfdd22a0f6226ff83ea6b9be6acc3f0923
LoneExile <Apinant-Moo@tutanota.com>
2024-06-09 00:17:50 +0700

    feat(mini.ai): targets.vim => mini.ai

--- lua/LoneExile/editor/motion/targets.lua
+++ lua/LoneExile/editor/motion/targets.lua
@@ -2,8 +2,34 @@ local M = {}
 
 M.setup = function()
   return {
-    'wellle/targets.vim',
+    'echasnovski/mini.ai',
     event = 'BufRead',
+    config = function()
+      local status_ok_code, mini = pcall(require, 'mini.ai')
+      if not status_ok_code then
+        return
+      end
+      local spec_treesitter = require('mini.ai').gen_spec.treesitter
+      local gen_spec = require('mini.ai').gen_spec
+
+      mini.setup({
+        custom_textobjects = {
+          o = spec_treesitter({
+            a = { '@conditional.outer', '@loop.outer' },
+            i = { '@conditional.inner', '@loop.inner' },
+          }),
+
+          -- Tweak function call to not detect dot in function name
+          F = gen_spec.function_call({ name_pattern = '[%w_]' }),
+
+          -- Function definition (needs treesitter queries with these captures)
+          f = gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+
+          -- Make `|` select both edges in non-balanced way
+          ['|'] = gen_spec.pair('|', '|', { type = 'non-balanced' }),
+        },
+      })
+    end,
   }
 end
 

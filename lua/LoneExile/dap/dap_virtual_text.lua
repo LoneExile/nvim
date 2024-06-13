9dbdc7e912c9628c3798eea07b51e3f734a91446
LoneExile <Apinant-Moo@tutanota.com>
2024-06-13 11:36:08 +0700

    fixed(dap-virtual-text): not load

--- lua/LoneExile/dap/dap_virtual_text.lua
+++ lua/LoneExile/dap/dap_virtual_text.lua
@@ -13,11 +13,13 @@ M.setup = function(_, _)
   -- settings.utils.setup_mappings('<leader>', M.wh_key.wh_mappings, _)
   return {
     'theHamsta/nvim-dap-virtual-text',
-    lazy = true,
+    cmd = { 'DapVirtualTextToggle' },
     -- event = { 'BufReadPre', 'InsertEnter' },
     dependencies = { 'mfussenegger/nvim-dap' },
     config = function()
-      require('nvim-dap-virtual-text').setup({})
+      require('nvim-dap-virtual-text').setup({
+        enabled = true,
+      })
     end,
   }
 end

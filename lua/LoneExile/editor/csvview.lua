34637ac92943ab8c3c69edee92b4dfb0a568c11a
LoneExile <Apinant-Moo@tutanota.com>
2024-11-13 14:44:16 +0700

    feat: load key base on ft

--- lua/LoneExile/editor/csvview.lua
+++ lua/LoneExile/editor/csvview.lua
@@ -1,25 +1,29 @@
 local M = {}
+
+M.ft = { 'csv' }
+
 M.keys = {
   {
     '<leader>uc',
     '<cmd>CsvViewToggle<cr>',
     desc = 'CSV View',
     cond = function()
-      return vim.bo.filetype == 'csv'
+      return vim.tbl_contains(M.ft, vim.bo.filetype)
     end,
-    hidden = function()
-      return vim.bo.filetype ~= 'csv'
-    end,
+    -- hidden = function()
+    --   return vim.bo.filetype ~= 'csv'
+    -- end,
   },
 }
 
-M.setup = function()
+M.setup = function(settings, _)
   return {
     'hat0uma/csvview.nvim',
-    ft = 'csv',
-    keys = M.keys,
+    ft = M.ft,
+    -- keys = M.keys,
     config = function()
       require('csvview').setup()
+      settings.utils.cond_keys(M.keys)
     end,
   }
 end

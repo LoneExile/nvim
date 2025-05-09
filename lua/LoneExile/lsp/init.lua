ae3df9a639f8e5bba9a91d3d30b09a64fe93f4d1
Apinant.us <me@apinant.dev>
2025-05-09 07:07:17 +0700

    fix: mason upgrade version
    https://www.reddit.com/r/neovim/comments/1kgu748/masonnvim_20_has_been_released/

--- lua/LoneExile/lsp/init.lua
+++ lua/LoneExile/lsp/init.lua
@@ -88,8 +88,8 @@ M.setup = function(_, location)
     keys = M.keys,
 
     dependencies = {
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { 'mason-org/mason.nvim', version = '1.11.0' },
+      { 'mason-org/mason-lspconfig.nvim', version = '1.32.0' },
       -- 'WhoIsSethDaniel/mason-tool-installer.nvim',
     },
     config = function()

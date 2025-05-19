093d773888437edada5ee86cc0a78a64823e74aa
Apinant.us <me@apinant.dev>
2025-05-19 07:41:35 +0700

    fix: format

--- lua/LoneExile/editor/markdown_preview.lua
+++ lua/LoneExile/editor/markdown_preview.lua
@@ -2,15 +2,15 @@ local M = {}
 
 M.setup = function()
   return {
-    "toppair/peek.nvim",
+    'toppair/peek.nvim',
     ft = { 'markdown' },
     cmd = { 'PeekOpen', 'PeekClose' },
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
     config = function()
-        require("peek").setup()
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+      require('peek').setup()
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
     end,
   }
 end

8987ecaa9302e14122365ff41dbdbb6c64ecb401
Apinant.us <me@apinant.dev>
2025-06-17 06:35:50 +0700

    feat: avante openrounter

--- lua/LoneExile/editor/ai/avante.lua
+++ lua/LoneExile/editor/ai/avante.lua
@@ -62,6 +62,7 @@ M.setup = function()
   vim.fn.setenv('OPENAI_API_KEY', vim.env.OPENAI_API_KEY)
   vim.fn.setenv('GEMINI_API_KEY', vim.env.GEMINI_API_KEY)
   vim.fn.setenv('TAVILY_API_KEY', vim.env.TAVILY_API_KEY)
+  vim.fn.setenv('OPENROUTER_API_KEY', vim.env.OPENROUTER_API_KEY)
   return {
     'yetone/avante.nvim',
     -- cmd = { 'AvanteAsk', 'AvanteEdit'  },
@@ -100,15 +101,23 @@ M.setup = function()
 
     config = function()
       require('avante').setup({
+        provider = "openrouter",
         providers = {
           claude = {
-            endpoint = "https://api.anthropic.com",
-            model = "claude-sonnet-4-20250514",
+            endpoint = 'https://api.anthropic.com',
+            model = 'claude-sonnet-4-20250514',
             extra_request_body = {
               max_tokens = 40000,
               temperature = 0,
             },
             -- disable_tools = true,
+          },
+          openrouter = {
+            __inherited_from = 'openai',
+            endpoint = 'https://openrouter.ai/api/v1',
+            -- model = 'anthropic/claude-opus-4',
+            model = 'google/gemini-2.5-pro-preview-06-05',
+            api_key_name = 'OPENROUTER_API_KEY',
           },
         },
         behaviour = {
@@ -140,13 +149,13 @@ M.setup = function()
         -- system_prompt as function ensures LLM always has latest MCP server state
         -- This is evaluated for every message, even in existing chats
         system_prompt = function()
-          local hub = require("mcphub").get_hub_instance()
-          return hub and hub:get_active_servers_prompt() or ""
+          local hub = require('mcphub').get_hub_instance()
+          return hub and hub:get_active_servers_prompt() or ''
         end,
         -- Using function prevents requiring mcphub before it's loaded
         custom_tools = function()
           return {
-            require("mcphub.extensions.avante").mcp_tool(),
+            require('mcphub.extensions.avante').mcp_tool(),
           }
         end,
       })

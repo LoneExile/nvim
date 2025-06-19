d5a21ceef7dd1cf1d706298ad146e07f304d5130
Apinant.us <me@apinant.dev>
2025-06-19 07:14:22 +0700

    chore: update

--- lua/LoneExile/cmp/init.lua
+++ lua/LoneExile/cmp/init.lua
@@ -13,6 +13,7 @@ M.setup = function(settings, location)
       { 'rafamadriz/friendly-snippets' },
       { 'jcha0713/cmp-tw2css' },
       { 'windwp/nvim-autopairs', dependencies = { 'nvim-treesitter/nvim-treesitter' }, event = 'InsertEnter' },
+      { 'SergioRibera/cmp-dotenv' },
     },
     config = function()
       local cmp_ok, cmp = pcall(require, 'cmp')
@@ -121,6 +122,23 @@ M.setup = function(settings, location)
           -- { name = 'cmp_tabnine'},
           { name = 'nvim_lua' },
           { name = 'cmp-tw2css' },
+          {
+            name = 'dotenv',
+            option = {
+              path = '.',
+              load_shell = true,
+              item_kind = cmp.lsp.CompletionItemKind.Variable,
+              eval_on_confirm = false,
+              show_documentation = true,
+              show_content_on_docs = true,
+              documentation_kind = 'markdown',
+              dotenv_environment = '.*',
+              file_priority = function(a, b)
+                -- Prioritizing local files
+                return a:upper() < b:upper()
+              end,
+            },
+          },
           { name = 'buffer', max_item_count = 5 },
           -- { name = 'calc' },
           -- { name = 'emoji' },

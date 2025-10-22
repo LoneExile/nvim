9481482ab32fadf346d82f1dd999ea82f93ad2d1
Apinant U-suwantim <Hello@Apinant.dev>
2025-10-22 13:41:09 +0700

    feat: add new md preview

--- lua/LoneExile/editor/ai/sidekick.lua
+++ lua/LoneExile/editor/ai/sidekick.lua
@@ -1,66 +1,77 @@
 local M = {}
 
 M.keys = {
-	{
-		"<leader>c",
-		"",
-		desc = "Copilot",
-		mode = "n",
-	},
-	{
-		"<tab>",
-		function()
-			-- if there is a next edit, jump to it, otherwise apply it if any
-			if not require("sidekick").nes_jump_or_apply() then
-				return "<Tab>" -- fallback to normal tab
-			end
-		end,
-		expr = true,
-		desc = "Goto/Apply Next Edit Suggestion",
-	},
-	{
-		"<leader>cs",
-		function()
-			require("sidekick.cli").toggle({ name = "copilot", focus = true })
-		end,
-		mode = { "n", "v" },
-		desc = "Sidekick Toggle CLI",
-	},
-	{
-		"<leader>cs",
-		function()
-			require("sidekick.cli").send({ name = "copilot", selection = true })
-		end,
-		mode = { "v" },
-		desc = "Sidekick Send Visual Selection",
-	},
-	-- {
-	--   "<leader>ap",
-	--   function() require("sidekick.cli").prompt() end,
-	--   mode = { "n", "v" },
-	--   desc = "Sidekick Select Prompt",
-	-- },
+  {
+    '<leader>c',
+    '',
+    desc = 'Copilot',
+    mode = 'n',
+  },
+  {
+    '<tab>',
+    function()
+      -- if there is a next edit, jump to it, otherwise apply it if any
+      if not require('sidekick').nes_jump_or_apply() then
+        return '<Tab>' -- fallback to normal tab
+      end
+    end,
+    expr = true,
+    desc = 'Goto/Apply Next Edit Suggestion',
+  },
+  {
+    '<leader>aa',
+    function()
+      require('sidekick.cli').toggle({ name = 'copilot', focus = true })
+    end,
+    mode = { 'n', 'v' },
+    desc = 'Sidekick Toggle CLI',
+  },
+  {
+    '<leader>ac',
+    function()
+      require('sidekick.cli').toggle({ name = 'claude', focus = true })
+    end,
+    mode = { 'n', 'v' },
+    desc = 'Sidekick Toggle Claude CLI',
+  },
+  {
+    '<leader>at',
+    function()
+      require('sidekick.cli').send({ msg = '{this}' })
+    end,
+    mode = { 'x', 'n' },
+    desc = 'Send This',
+  },
+  {
+    '<leader>af',
+    function()
+      require('sidekick.cli').send({ msg = '{file}' })
+    end,
+    desc = 'Send File',
+  },
 }
 
 M.setup = function()
-	return {
-		"folke/sidekick.nvim",
-		keys = M.keys,
-		opts = {
-			cli = {
-				mux = {
-					backend = "tmux",
-					enabled = true,
-				},
-				tools = {
-					copilot = {
-						cmd = { "copilot", "--allow-all-tools" },
-						url = "https://github.com/github/copilot-cli",
-					},
-				},
-			},
-		},
-	}
+  return {
+    'folke/sidekick.nvim',
+    keys = M.keys,
+    opts = {
+      cli = {
+        mux = {
+          backend = 'tmux',
+          enabled = true,
+        },
+        tools = {
+          copilot = {
+            cmd = { 'copilot', '--allow-all-tools' },
+            url = 'https://github.com/github/copilot-cli',
+          },
+          -- claude = { cmd = { 'claude', '--dangerously-skip-permissions' } },
+          claude = { cmd = { 'claude' } },
+        },
+      },
+    },
+  }
 end
 
 return M
